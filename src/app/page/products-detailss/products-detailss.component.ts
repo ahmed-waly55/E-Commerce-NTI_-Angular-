42af4e4a707db7192fe0d3322220a653057087d2
ahmed waly <ahmedwaly074@gmail.com>
2024-12-26 01:54:09 +0200

    products page

---
+++ src/app/page/products-detailss/products-detailss.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-detailss',
+  standalone: true,
+  imports: [],
+  templateUrl: './products-detailss.component.html',
+  styleUrl: './products-detailss.component.scss'
+})
+export class ProductsDetailssComponent {
+
+}
