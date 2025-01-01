ea39a42d0fc84f0154b573d3a14e5da9b2888c4b
Mohamed Bayoumi <mohamedelsayed10zm@gmail.com>
2025-01-02 01:12:16 +0200

    products

--- src/app/page/products/products.component.ts
+++ src/app/page/products/products.component.ts
@@ -1,12 +1,53 @@
+import { Pagination } from './../../interfaces/pagination';
 import { Component } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+import { CurrencyPipe, DecimalPipe } from '@angular/common';
+import { HeaderComponent } from '../../components/header/header.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, CurrencyPipe, DecimalPipe, RouterLink],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
+  pagination: Pagination = {};
+  products: any[] = [];
+  private subscription: any;
+  private page: number = 1;
+  private limit: number = 20;
+  private search: string = '';
 
+  constructor(private _ProductsService: ProductsService) {}
+
+  loadProducts() {
+    this.subscription = this._ProductsService
+      .getProducts(this.page, this.limit, 'category,name', this.search)
+      .subscribe({
+        next: (res: any) => {
+          this.products = res.data;
+          this.pagination = res.pagination;
+        },
+      });
+  }
+
+  searchProducts(value: string) {
+    this.search = value;
+    this.loadProducts();
+  }
+
+  changePage(page: number) {
+    this.page = page;
+    this.loadProducts();
+  }
+
+  ngOnInit() {
+    this.loadProducts();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
